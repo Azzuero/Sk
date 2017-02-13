984d58267d458fea5ba98909fbc1660ada966047
mynikgabriel@yandex.ru <ford1transit>
2017-02-13 17:00:24 +0200

    EMPLOYEE class description

--- src/main/java/TRAINING/EMPLOYEE.java
+++ src/main/java/TRAINING/EMPLOYEE.java
@@ -1,4 +1,31 @@
 package TRAINING;
 
 public class EMPLOYEE {
+    private String CRMD;
+    private String AMBIENT;
+    private String POSITION;
+
+    public String getCRMD() {
+        return CRMD;
+    }
+
+    public void setCRMD(String CRMD) {
+        this.CRMD = CRMD;
+    }
+
+    public String getAMBIENT() {
+        return AMBIENT;
+    }
+
+    public void setAMBIENT(String AMBIENT) {
+        this.AMBIENT = AMBIENT;
+    }
+
+    public String getPOSITION() {
+        return POSITION;
+    }
+
+    public void setPOSITION(String POSITION) {
+        this.POSITION = POSITION;
+    }
 }

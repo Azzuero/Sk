6eda1c91cd00811777565b2105f466e828d14b9c
DinuMihnea` <dinoom97@gmail.com>
2017-02-13 17:54:01 +0200

    util hibernate

---
+++ src/main/java/UTIL/HibernateUtil.java
@@ -0,0 +1,26 @@
+package UTIL;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try{
+            return new Configuration().configure().buildSessionFactory();
+        }catch (Throwable e){
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void close() {
+        getSessionFactory().close();
+    }
+}

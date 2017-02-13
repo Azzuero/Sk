da5242d2951332ab64e962778257f6d20fca1f64
mynikgabriel@yandex.ru <ford1transit>
2017-02-13 17:27:45 +0200

    Second Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Fuck first Commit");
+        System.out.println("Fuck second Commit");
         System.out.println("Hi Bleas!");
     }
 }

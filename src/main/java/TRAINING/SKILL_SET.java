2aaf8ee42d9adfbb4be01af5feaaa2b96e4f8d16
mynikgabriel@yandex.ru <ford1transit>
2017-02-14 11:55:26 +0200

    SKILL_SET onetomany

--- src/main/java/TRAINING/SKILL_SET.java
+++ src/main/java/TRAINING/SKILL_SET.java
@@ -4,54 +4,49 @@ import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
+import java.sql.Time;
 import java.util.Date;
 
 @Entity
 public class SKILL_SET {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @Fetch(FetchMode.SELECT)
-    @JoinColumn(name = "EMPLOYEE", nullable = false)
-    private EMPLOYEE CRMD;
+    private Time assignedDate;
+    private String assigneeId;
+    private EMPLOYEE employeeByCrmd;
+    private SKILL skillBySkillId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @Fetch(FetchMode.SELECT)
-    @JoinColumn(name = "SKILL_ID", nullable = false)
-    private SKILL SKILL_ID;
-
-    private Date ASSIGNET_DATE;
-
-    private String ASSIGNEE_ID;
-
-    public EMPLOYEE getCRMD() {
-        return CRMD;
+    public Time getAssignedDate() {
+        return assignedDate;
     }
 
-    public void setCRMD(EMPLOYEE CRMD) {
-        this.CRMD = CRMD;
+    public void setAssignedDate(Time assignedDate) {
+        this.assignedDate = assignedDate;
     }
 
-    public SKILL getSKILL_ID() {
-        return SKILL_ID;
+    public String getAssigneeId() {
+        return assigneeId;
     }
 
-    public void setSKILL_ID(SKILL SKILL_ID) {
-        this.SKILL_ID = SKILL_ID;
+    public void setAssigneeId(String assigneeId) {
+        this.assigneeId = assigneeId;
+    }
+    @ManyToOne
+    @JoinColumn(name = "CRMD", referencedColumnName = "CRMD")
+    public EMPLOYEE getEmployeeByCrmd() {
+        return employeeByCrmd;
     }
 
-    public Date getASSIGNET_DATE() {
-        return ASSIGNET_DATE;
+    public void setEmployeeByCrmd(EMPLOYEE employeeByCrmd) {
+        this.employeeByCrmd = employeeByCrmd;
     }
 
-    public void setASSIGNET_DATE(Date ASSIGNET_DATE) {
-        this.ASSIGNET_DATE = ASSIGNET_DATE;
+    @ManyToOne
+    @JoinColumn(name = "SKILL_ID", referencedColumnName = "ID")
+    public SKILL getSkillBySkillId() {
+        return skillBySkillId;
     }
 
-    public String getASSIGNEE_ID() {
-        return ASSIGNEE_ID;
-    }
-
-    public void setASSIGNEE_ID(String ASSIGNEE_ID) {
-        this.ASSIGNEE_ID = ASSIGNEE_ID;
+    public void setSkillBySkillId(SKILL skillBySkillId) {
+        this.skillBySkillId = skillBySkillId;
     }
 }

32fd981eb3909174f0d116b210164c0e54e34745
DinuMihnea` <dinoom97@gmail.com>
2017-02-15 14:31:34 +0200

    gotowa

--- src/main/java/UTIL/Utilites.java
+++ src/main/java/UTIL/Utilites.java
@@ -51,7 +51,7 @@ public class Utilites {
     }
 
     public static Boolean verifySkillParent(SKILL_SET assSkill_set, SKILL_SET emplSkill_set){
-        boolean b = false;
+            boolean b = false;
         if(assSkill_set.getSkillBySkillId().getParentId() == emplSkill_set.getSkillBySkillId().getParentId()){
             if (Long.parseLong(assSkill_set.getSkillBySkillId().getName()) > Long.parseLong(emplSkill_set.getSkillBySkillId().getName())){
                 b=true;
@@ -59,50 +59,60 @@ public class Utilites {
         }
         return b;
     }
-    public static Boolean lowerSkillAfter(Date date, Set<SKILL_SET> skill_sets){
-        boolean b = false;
-        for (SKILL_SET next: skill_sets) {
-            if (date.compareTo(next.getAssignedDate()) > 0){
-                b = true;
-            }
-
+        public static Boolean lowerSkillAfter(Date date, Set<SKILL_SET> skill_sets){
+            boolean b = false;
+            for (SKILL_SET next: skill_sets) {
+                if (date.compareTo(next.getAssignedDate()) > 0){
+                    b = true;
+                }
         }
 
         return b;
     }
     public static boolean isAbleToAttributeSkillFor(EMPLOYEE employee, EMPLOYEE assignee, SKILL emplSkill, SKILL assSkill, SKILL_SET emplSkillSet, SKILL_SET assSkillSet){
 
+        boolean result = true;
+
         //different persons
         if (employee.getCrmd().equals(assignee.getCrmd())){
             System.err.println("ERROR: Assignee and employee should be different persons!!!");
+            result = false;
         }
         //Position
         else if (getEmployeeRank(employee.getPosition()) >= getEmployeeRank(assignee.getPosition())){
             System.err.println("ERROR: Position of assignee must be greater than position of employee!!!");
+            result = false;
         }
         //Ambient
         else if (!employee.getAmbient().equals(assignee.getAmbient())){
             System.err.println("ERROR: Assignee and employee must be in the same ambient!!!");
+            result = false;
         }
         //Skill
         else if (verifySkillWiden(emplSkill) == false){
             System.err.println("ERROR: Skill cannot be widen (not higher in hierarchy of skills)!!!");
+            result = false;
         }
         //Lowered
         else if (verifySkillLower(emplSkill, emplSkillSet, employee.getCrmd()) == false){
             System.err.println("ERROR: Skill cannot be lowered!!!");
+            result = false;
         }
         //Parent
         else if (verifySkillParent(assSkillSet, emplSkillSet) == false){
             System.err.println("ERROR: Assignee must have higher Skill than employee!!!");
+            result = false;
         }
+        //Date
         else if (lowerSkillAfter(emplSkillSet.getAssignedDate(), Records.skill_sets()) == false){
             System.err.println("ERROR: Incorect Date!!!");
+            result = false;
         }
 
         else{
             System.out.println("Its OK!");
+            result = true;
         }
-    return  false;
+    return  result;
     }
 }
--- src/test/java/TEST.java
+++ src/test/java/TEST.java
@@ -1,5 +1,31 @@
-/**
- * Created by crm0035 on 2/14/2017.
- */
+import UTIL.Records;
+import UTIL.Utilites;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TEST {
+    Utilites utilites = new Utilites();
+
+    @Test
+    public void testAble() throws Exception {
+
+
+        assertEquals("isAbleToAttributeSkillFor it's FUCK; Caused By:",false, utilites.isAbleToAttributeSkillFor( Records.employee1,  Records.employee3,  Records.skill7, Records.skill7, Records.skill_set3, Records.skill_set2));
+    }
+
+    @Test
+    public void employeeRank() throws Exception{
+
+
+        assertEquals("getEmployeerank it's FUCK; Caused By:",3, utilites.getEmployeeRank(Records.employee3.getPosition()));
+    }
+
+    @Test
+    public void lowerSkillAfter() {
+
+       assertEquals("lowerSkillAfter it's FUCK; Caused By:",false,utilites.lowerSkillAfter(Records.skill_set3.getAssignedDate(), Records.skill_sets()));
+
+    }
 }
+

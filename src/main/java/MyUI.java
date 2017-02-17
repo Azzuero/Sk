7d8763885d8fd9b0a59063c9ed161369ab97cc91
mynikgabriel@yandex.ru <ford1transit>
2017-02-17 16:31:04 +0200

    Vaadin init

--- src/main/java/MyUI.java
+++ src/main/java/MyUI.java
@@ -2,7 +2,10 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.DateField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -19,7 +22,13 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-
+        VerticalLayout Principal_Page = new VerticalLayout();
+        ComboBox combocrmd = new ComboBox("CRMD");
+        ComboBox comboskill = new ComboBox("Skill");
+        ComboBox comboAsignee = new ComboBox("Asignee CRMD");
+        DateField dateSkill = new DateField();
+        Principal_Page.addComponents(combocrmd, comboskill, comboAsignee, dateSkill);
+        setContent(Principal_Page);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
